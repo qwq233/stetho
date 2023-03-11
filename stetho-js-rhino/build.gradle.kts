05650db29e6ce75449af643a9eb30e01747dda62
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-03-11 19:35:54 +0800

    Prepare for release
    Signed-off-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>

--- stetho-js-rhino/build.gradle.kts
+++ stetho-js-rhino/build.gradle.kts
@@ -30,8 +30,33 @@ afterEvaluate {
                 artifactId = project.properties["POM_ARTIFACT_ID"] as String
                 version = project.properties["VERSION_NAME"] as String
 
-                afterEvaluate {
-                    from(components["release"])
+                from(components["release"])
+
+                pom {
+                    name.set(project.properties["POM_NAME"] as String)
+                    description.set("Stetho Debugging Platform for Android")
+                    url.set("https://github.com/5ec1cff/stetho")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/5ec1cff/stetho/blob/master/LICENSE")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("facebook")
+                            name.set("facebook")
+                        }
+                        developer {
+                            id.set("a13e300")
+                            name.set("5ec1cff")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/5ec1cff/stetho.git")
+                        developerConnection.set("scm:git:git@github.com:5ec1cff/stetho.git")
+                        url.set("https://github.com/5ec1cff/stetho.git")
+                    }
                 }
             }
         }
@@ -49,5 +74,3 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
 }
-
-// apply from: rootProject.file("release.gradle")
--- stetho/build.gradle.kts
+++ stetho/build.gradle.kts
@@ -32,9 +32,33 @@ afterEvaluate {
                 groupId = project.properties["GROUP"] as String
                 artifactId = project.properties["POM_ARTIFACT_ID"] as String
                 version = project.properties["VERSION_NAME"] as String
+                from(components["release"])
 
-                afterEvaluate {
-                    from(components["release"])
+                pom {
+                    name.set(project.properties["POM_NAME"] as String)
+                    description.set("Stetho Debugging Platform for Android")
+                    url.set("https://github.com/5ec1cff/stetho")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://github.com/5ec1cff/stetho/blob/master/LICENSE")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("facebook")
+                            name.set("facebook")
+                        }
+                        developer {
+                            id.set("a13e300")
+                            name.set("5ec1cff")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/5ec1cff/stetho.git")
+                        developerConnection.set("scm:git:git@github.com:5ec1cff/stetho.git")
+                        url.set("https://github.com/5ec1cff/stetho.git")
+                    }
                 }
             }
         }
